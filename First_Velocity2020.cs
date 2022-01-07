8b5c102c552e74bc0f8c7e1caf786a390ddb6aeb
is0484sh <97268159+is0484sh@users.noreply.github.com>
2022-01-07 11:47:33 +0900

    Add files via upload

---
+++ First_Velocity2020.cs
@@ -0,0 +1,19 @@
+﻿//Update for Unity2019 on 5.21.2020
+
+using UnityEngine;
+using System.Collections;
+
+public class First_Velocity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+//		rigidbody.velocity = new Vector3 (0.0f,2.0f,0.0f);
+		GetComponent<Rigidbody>().velocity = new Vector3 (0.0f,2.0f,0.0f);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ GetKey.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GetKey: MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey("up")) {
+			transform.Rotate (1f,0f,0f);
+		}
+		if (Input.GetKey("down")) {
+			transform.Rotate (-1f,0f,0f);
+		}
+		if (Input.GetKey("right")) {
+			transform.Rotate (0f,0f,-1f);
+		}
+		if (Input.GetKey("left")) {
+			transform.Rotate (0f,0f,1f);
+		}
+	}
+}
